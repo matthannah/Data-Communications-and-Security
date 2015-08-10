dfa112a7d918769b4fbd7d3f85842129db2f25e3
rhysh90 <rhysh90@gmail.com>
2015-08-10 12:29:45 +1000

    Client changes
    added address to client

--- Lab 2/TCPClient/TCPClient.java
+++ Lab 2/TCPClient/TCPClient.java
@@ -15,7 +15,8 @@ class TCPClient
    String sentence;
    String modifiedSentence;
    BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-   Socket clientSocket = new Socket("localhost", 6789);
+   InetAddress address = InetAddress.getByName("10.1.51.131");
+   Socket clientSocket = new Socket(address, 6789);
    DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
    sentence = inFromUser.readLine();
