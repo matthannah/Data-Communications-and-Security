34ce04b78fbdbd311135973db9a0e5e2110d9e0b
matthannah <mryanhannah@gmail.com>
2015-10-16 16:24:07 +1100

    updated port scan

--- Assignment 2 Matt/Portscan/Test.java
+++ Assignment 2 Matt/Portscan/Test.java
@@ -24,20 +24,11 @@ public class Test
      */
     public static void main(String [] args) throws Exception
     {  
-        final ExecutorService es = Executors.newFixedThreadPool(20); // start 20 threads
         final String ip = args[0]; // ip address which is received via the command line
         final Integer range = Integer.valueOf(args[1]);
         final List<Future<Boolean>> futures = new ArrayList<>(); // list of booleans
         List<Integer> ports = new ArrayList<>(); // list of integers of ports to be scanned
-        
-        Random random = new Random(); // creates a random object
-        int min = 100;
-        int max = 500;
-        int randomNumber = random.nextInt(max - min) + min; // assign a random number between range
-                                                            // min and max
-        final int timeout = randomNumber; // set the timeout equal to the random number
-
-        
+        final ExecutorService es = Executors.newFixedThreadPool(20); // start 20 threads
         //create a list of ips to be scanned
         ArrayList<String> ips = new ArrayList<String>();
         //add the first ip entered by the user
@@ -45,58 +36,78 @@ public class Test
         //get the last number of the ip address
         String parts[] = ip.split("\\.");
         //add each ip to the list
-        for (int i = 1; i <= range; i++)
+        for (int i = 1; i < range; i++)
         {
             Integer lastNumber = Integer.valueOf(parts[3]) + i;
             ips.add(parts[0] + "." + parts[1] + "." + parts[2] + "." + lastNumber);
         }
         
+        // this loop adds all the ports to be scanned into a list
+        // ports to be scanned is determined by the user entering the
+        // range in the command line
+        for (int i =  Integer.parseInt(args[2]); i <= Integer.parseInt(args[3]); i++)
+        {
+            ports.add(i); //adds port to list
+        }
+        
         //if the ip addresses are in the subnet
         if(inSubnet(ips))
         {
-            // this loop adds all the ports to be scanned into a list
-            // ports to be scanned is determined by the user entering the
-            // range in the command line
-            for (int i =  Integer.parseInt(args[2]); i <= Integer.parseInt(args[3]); i++)
+            for (String address : ips)
             {
-                ports.add(i); //adds port to list
-            }
-            
-            Collections.shuffle(ports); // randomize the list, effectively randomizes the order in which
-                                        // ports are scanned
-            
-                      
-            if (ports.size() > 1 && Integer.parseInt(args[2]) > 0)
-            {
-                for (int i =  Integer.parseInt(args[2]); i <= Integer.parseInt(args[3]); i++) 
+                Random random = new Random(); // creates a random object
+                int min = 100;
+                int max = 500;
+                int randomNumber = random.nextInt(max - min) + min; // assign a random number between range
+                                                                    // min and max
+                final int timeout = randomNumber; // set the timeout equal to the random number
+                
+                Collections.shuffle(ports); // randomize the list, effectively randomizes the order in which
+                                            // ports are scanned
+                                            
+                ArrayList<Integer> open = new ArrayList<>();
+                
+                          
+                if (ports.size() > 1 && Integer.parseInt(args[2]) > 0)
                 {
-                    System.out.println("" + ports.get(i-1)); //prints the port about to be scanned
-                    futures.add(portIsOpen(es, ip, ports.get(i-1), timeout)); // add a future boolean
-                                                                              // to the list
+                    for (int i =  Integer.parseInt(args[2]); i <= Integer.parseInt(args[3]); i++) 
+                    {
+                        //System.out.println("" + ports.get(i-1)); //prints the port about to be scanned
+                        futures.add(portIsOpen(es, address, ports.get(i-1), timeout, open)); // add a future boolean
+                                                                                  // to the list
+                    }
                 }
-            }
-           es.shutdown(); // shutdown threads gracefully - meaning that they will finish their tasks 
-                           // and then terminate, in our case they will finish establishing connections
-                           // first
                            
-            int openPorts = 0; // number of openPorts currently
-    
-            for (final Future<Boolean> f : futures) //for all data in the list of futures
-            {
-                if (f.get()) // return true if port is open
+                int openPorts = 0; // number of openPorts currently
+        
+                for (final Future<Boolean> f : futures) //for all data in the list of futures
                 {
-                    openPorts++; // add one to the open port count
+                    if (f.get()) // return true if port is open
+                    {
+                        openPorts++; // add one to the open port count
+                    }
                 }
+               
+                
+                // prints message to system detailing scan result
+                System.out.println("Scan Results for " + address + ": " + openPorts + " open ports, probed with timeout of " + timeout + "ms");  
+                for (Integer p : open)
+                {
+                    System.out.println(p + " open");
+                }
+                System.out.println("");
+                futures.clear();
             }
-            
-            // prints message to system detailing scan result
-            System.out.println("There are " + openPorts + " open ports on host " + ip +  
-            " probed with a timeout of " + timeout + "ms");
         }
         else
         {
             System.out.println("IP addresses entered are not in the subnet");
         }
+        es.shutdown(); // shutdown threads gracefully - meaning that they will finish their tasks 
+                       // and then terminate, in our case they will finish establishing connections
+                       // first
+                       
+        System.out.println("Scan Complete");
     }
        
      /**
@@ -111,7 +122,7 @@ public class Test
      *                          established return false
      */
     public static Future<Boolean> portIsOpen(final ExecutorService es, 
-    final String ip, final int port, final int timeout) 
+    final String ip, final int port, final int timeout, ArrayList<Integer> open) 
     {
         return es.submit(new Callable<Boolean>() 
         {
@@ -122,7 +133,7 @@ public class Test
                     Socket socket = new Socket(); // create a new socket object
                     socket.connect(new InetSocketAddress(ip, port), timeout); // attempt to connect to a socket at specified ip and port
                     socket.close(); // close the socket
-                    System.out.println("open port found " + port); // if a connection was established (no exception thrown) print success to screen
+                    open.add(port);
                     return true; 
                 } 
                 catch (Exception ex) // an exception is thrown if the connection process exceeds the timeout time
@@ -157,7 +168,6 @@ public class Test
         try
         {
             local = (Inet4Address) InetAddress.getLocalHost();
-            System.out.println("local network: " + local);
             NetworkInterface ni = NetworkInterface.getByInetAddress(local);
             //bitsSubnet = ni.getInterfaceAddresses().get(0).getNetworkPrefixLength();
             List<InterfaceAddress> ia = ni.getInterfaceAddresses();
